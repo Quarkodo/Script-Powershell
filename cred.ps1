f81d6f5f5cb50975235e4a22e6f7533c986ae6ba
Quarkodo <gerard.aroquianadin@gmail.com>
2017-01-06 10:14:35 +0100

    Add files via upload

---
+++ cred.ps1
@@ -0,0 +1,10 @@
+﻿$username = Get-Content c:\listeusername.txt;
+
+for($i=0;$i -lt $a.Length;$i++){
+
+$password = Get-Content c:\listepassword.txt;
+$secureStringPwd = $password[$i] | ConvertTo-SecureString -AsPlainText -Force
+    for($e=0;$e -lt $password.Length;$e++){
+    write $username[$i] $secureStringPwd[$e]
+    }
+}
